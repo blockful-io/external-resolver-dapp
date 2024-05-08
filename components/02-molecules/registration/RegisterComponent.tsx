aeb365fd111840d2996588598682927f0aecb230
eduramme <eduramme@gmail.com>
2024-05-08 13:43:52 -0300

    create remaining bodies

--- components/02-molecules/registration/RegisterComponent.tsx
+++ components/02-molecules/registration/RegisterComponent.tsx
@@ -1,5 +1,28 @@
-"use client";
+import BackButton from "@/components/01-atoms/BackButton";
+import NextButton from "@/components/01-atoms/NextButton";
 
-export default function RegisterComponent() {
-  return <div className="bg-pink-500 p-4">RegisterComponent</div>;
+interface RegisterComponentProps {
+  handlePreviousStep: () => void;
+  handleNextStep: () => void;
+}
+
+export default function RegisterComponent({
+  handlePreviousStep,
+  handleNextStep,
+}: RegisterComponentProps) {
+  return (
+    <div className="flex flex-col gap-[44px] justify-start items-start">
+      <BackButton onClick={handlePreviousStep} />
+      <div className="max-w-[500px] w-full flex items-start flex-col gap-4">
+        <h3 className="text-[72px]">🎉</h3>
+        <h3 className="text-start text-[34px] font-medium">
+          Congrats! You&apos;re now owner of isadoranunes.eth
+        </h3>
+        <p className="text-gray-500 text-left text-[16px]">
+          Your name was successfully registered, you can now view and manage it.
+        </p>
+      </div>
+      <NextButton onClick={handleNextStep} />
+    </div>
+  );
 }
