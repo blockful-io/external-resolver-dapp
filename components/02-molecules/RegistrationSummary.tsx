c18410915316cea4700655afd83b8a98e28c89cf
eduramme <eduramme@gmail.com>
2024-05-08 14:01:35 -0300

    resolve requested changes

--- components/02-molecules/RegistrationSummary.tsx
+++ components/02-molecules/RegistrationSummary.tsx
@@ -1,28 +1,8 @@
-"use client";
-
-import { Typography } from "@ensdomains/thorin";
+import { CurrencyToggle, Typography } from "@ensdomains/thorin";
 import CartIcon from "../01-atoms/icons/cart-icon";
 import InfoCircleIcon from "../01-atoms/icons/info-circle";
-import { useState } from "react";
-import { useNameRegistration } from "@/lib/name-registration/useNameRegistration";
-
-interface CurrencyData {
-  currency: "usd" | "eth";
-}
-
-const Currency: Record<string, CurrencyData["currency"]> = {
-  usd: "usd",
-  eth: "eth",
-};
 
 export default function RegistrationSummary() {
-  const [currency, setCurrency] = useState<CurrencyData["currency"]>(
-    Currency.usd
-  );
-
-  const { nameRegistrationData } = useNameRegistration();
-
-  console.log(nameRegistrationData);
   return (
     <div className="w-[474px] border border-gray-200 rounded-[12px] flex flex-col overflow-hidden">
       <div className="p-6 border-b border-gray-200 flex flex-col items-start gap-6">
@@ -44,34 +24,7 @@ export default function RegistrationSummary() {
               <p className="text-gray-400 text-sm font-semibold">65 gwei</p>
             </div>
           </div>
-          <div
-            className={`p-1 border border-gray-200 font-semibold bg-gray-50 flex items-center justify-center rounded-[12px] `}
-          >
-            <div
-              className={`cursor-pointer py-2 px-3 font-semibold rounded-[8px] transition-colors duration-200 ${
-                currency === Currency.usd
-                  ? "bg-blue-500 text-white"
-                  : "text-gray-400 bg-gray-50"
-              }`}
-              onClick={() => {
-                setCurrency(Currency.usd);
-              }}
-            >
-              USD
-            </div>
-            <div
-              className={`cursor-pointer py-2 px-3 font-semibold rounded-[8px] transition-colors duration-200 ${
-                currency === Currency.eth
-                  ? "bg-blue-500 text-white"
-                  : "text-gray-400 bg-gray-50"
-              }`}
-              onClick={() => {
-                setCurrency(Currency.eth);
-              }}
-            >
-              ETH
-            </div>
-          </div>
+          <CurrencyToggle />
         </div>
         <div className="flex w-full justify-between items-center">
           <p>1 year registration</p>
