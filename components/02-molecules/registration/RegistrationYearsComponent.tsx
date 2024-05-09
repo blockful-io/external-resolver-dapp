63003cf983291feb890b03c1d88846383dec6085
eduramme <eduramme@gmail.com>
2024-05-09 14:52:23 -0300

    create registration years state

--- components/02-molecules/registration/RegistrationYearsComponent.tsx
+++ components/02-molecules/registration/RegistrationYearsComponent.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import BackButton from "@/components/01-atoms/BackButton";
 import NextButton from "@/components/01-atoms/NextButton";
 import { PlusSVG, MinusSVG } from "@ensdomains/thorin";
+import { useState } from "react";
 
 interface RegistrationYearsComponentProps {
   handlePreviousStep: () => void;
@@ -11,6 +14,16 @@ export function RegistrationYearsComponent({
   handlePreviousStep,
   handleNextStep,
 }: RegistrationYearsComponentProps) {
+  const [years, setYears] = useState<number>(1);
+
+  const handlePlusButtonClick = () => {
+    setYears(years + 1);
+  };
+
+  const handleMinusButtonClick = () => {
+    setYears(years - 1);
+  };
+
   return (
     <div className="flex flex-col gap-[44px] justify-start items-start">
       <BackButton onClick={handlePreviousStep} />
@@ -20,14 +33,21 @@ export function RegistrationYearsComponent({
           How many years do you want to register this domain?
         </h3>
         <div className="flex">
-          <div className="flex justify-between items-center border border-gray-200 rounded-[8px]">
-            <div className="p-4 border-r border-gray-200">
+          <div className="flex justify-between items-center border border-gray-200 rounded-[8px] overflow-hidden">
+            <button
+              disabled={years === 1}
+              onClick={handleMinusButtonClick}
+              className="p-4 border-r border-gray-200 hover:bg-gray-50"
+            >
               <MinusSVG className="w-3 h-3 text-gray-500" />
-            </div>
-            <div className="w-[120px]">1</div>
-            <div className="p-4 border-l border-gray-200">
+            </button>
+            <div className="w-[120px]">{years}</div>
+            <button
+              onClick={handlePlusButtonClick}
+              className="p-4 border-l border-gray-200 hover:bg-gray-50"
+            >
               <PlusSVG className="w-3 h-3 text-gray-500" />
-            </div>
+            </button>
           </div>
         </div>
       </div>
