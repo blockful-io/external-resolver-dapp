db66de12527fb218d7c00bb1ec42a11880ec4594
eduramme <eduramme@gmail.com>
2024-05-15 16:00:01 -0300

    create manage domains page

---
+++ app/manage/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterNamePage() {
+  return (
+    <div className="text-black flex h-full flex-col items-center justify-start bg-white">
+      <div className="w-full px-[60px] py-10 flex justify-start">
+        <h3 className="text-start text-[26px]"> Manage domains</h3>
+      </div>
+    </div>
+  );
+}
--- components/01-atoms/DappHeader.tsx
+++ components/01-atoms/DappHeader.tsx
@@ -2,8 +2,8 @@ import { Button, WalletSVG } from "@ensdomains/thorin";
 
 export const DappHeader = () => {
   return (
-    <div className="w-full h-20 bg-white px-6 flex justify-between items-center shadow-sm">
-      <div className="flex items-center justify-center gap-2.5">
+    <div className="w-full h-20 bg-white px-6 flex justify-between items-center shadow z-20">
+      <div className="flex items-center justify-center gap-2.5 shadow-2xl ">
         <div className=" h-6 w-6 bg-gradient-ens rounded-full" />
         <p className="text-[16px] font-bold text-black">DomainResolver</p>
       </div>
