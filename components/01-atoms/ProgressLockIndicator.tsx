fc1371702c44c8e8ca33bacbd04dc28b8f9c8116
Eduardo <eduramme@gmail.com>
2024-05-23 15:19:24 -0300

    Update components/01-atoms/ProgressLockIndicator.tsx
    Co-authored-by: frankind.eth <49823133+FrancoAguzzi@users.noreply.github.com>

--- components/01-atoms/ProgressLockIndicator.tsx
+++ components/01-atoms/ProgressLockIndicator.tsx
@@ -43,7 +43,7 @@ const ProgressLockIndicator = ({ timerDone }: ProgressLockIndicatorProps) => {
           timerDone ? "opacity-0" : "opacity-100"
         }`}
       >
-        <p className="text-[72px]">🔒</p>
+        <p className="text-7xl">🔒</p>
       </div>
       <div
         className={`absolute inset-0 flex items-center justify-center transition-opacity duration-200 ${
