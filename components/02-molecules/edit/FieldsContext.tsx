fcfa124c1ca187bd5c075ab17d890d283894ea18
eduramme <eduramme@gmail.com>
2024-05-24 17:59:11 -0300

    update edit fields saving logic

--- components/02-molecules/edit/FieldsContext.tsx
+++ components/02-molecules/edit/FieldsContext.tsx
@@ -3,6 +3,9 @@ import React, { createContext, useContext, useState, ReactNode } from "react";
 
 interface FieldsContextType {
   fields: Record<Tab, Field[]>;
+  initialFields: Record<Tab, Field[]>;
+  setInitialFields: (fields: Record<Tab, Field[]>) => void;
+  setFields: (fields: Record<Tab, Field[]>) => void;
   addField: (tab: Tab, fieldName: string) => void;
   updateField: (tab: Tab, index: number, newValue: string) => void;
 }
@@ -14,7 +17,7 @@ interface FieldsProviderProps {
 const FieldsContext = createContext<FieldsContextType | undefined>(undefined);
 
 const FieldsProvider: React.FC<FieldsProviderProps> = ({ children }) => {
-  const [fields, setFields] = useState<Record<Tab, Field[]>>({
+  const [fields, setFieldsState] = useState<Record<Tab, Field[]>>({
     [Tab.Profile]: [
       { label: "Website", placeholder: "https://coolcats.com", value: "" },
     ],
@@ -32,13 +35,43 @@ const FieldsProvider: React.FC<FieldsProviderProps> = ({ children }) => {
     ],
   });
 
+  const [initialFields, setInitialFieldsState] = useState<Record<Tab, Field[]>>(
+    {
+      [Tab.Profile]: [
+        { label: "Website", placeholder: "https://coolcats.com", value: "" },
+      ],
+      [Tab.Accounts]: [
+        { label: "Email", placeholder: "mail@mail.com", value: "" },
+        { label: "Twitter", placeholder: "@twitter", value: "" },
+        { label: "Linkedin", placeholder: "/linkedin", value: "" },
+      ],
+      [Tab.Addresses]: [
+        { label: "ETH", placeholder: "mail@mail.com", value: "" },
+      ],
+      [Tab.Others]: [
+        { label: "Content hash", placeholder: "", value: "" },
+        { label: "ABI", placeholder: "", value: "" },
+      ],
+    }
+  );
+
+  const setInitialFields = (fields: Record<Tab, Field[]>) => {
+    const deepCopiedFields = { ...fields };
+    setInitialFieldsState(deepCopiedFields);
+  };
+
+  const setFields = (fields: Record<Tab, Field[]>) => {
+    const deepCopiedFields = { ...fields };
+    setFieldsState(deepCopiedFields);
+  };
+
   const addField = (tab: Tab, fieldName: string) => {
     const newField: Field = {
       label: fieldName,
       placeholder: "",
       value: "",
     };
-    setFields((prevFields) => ({
+    setFieldsState((prevFields) => ({
       ...prevFields,
       [tab]: [...prevFields[tab], newField],
     }));
@@ -47,14 +80,23 @@ const FieldsProvider: React.FC<FieldsProviderProps> = ({ children }) => {
   const updateField = (tab: Tab, index: number, newValue: string) => {
     const updatedFields = [...fields[tab]];
     updatedFields[index].value = newValue;
-    setFields((prevFields) => ({
+    setFieldsState((prevFields) => ({
       ...prevFields,
       [tab]: updatedFields,
     }));
   };
 
   return (
-    <FieldsContext.Provider value={{ fields, addField, updateField }}>
+    <FieldsContext.Provider
+      value={{
+        fields,
+        initialFields,
+        setInitialFields,
+        setFields,
+        addField,
+        updateField,
+      }}
+    >
       {children}
     </FieldsContext.Provider>
   );
