61042d2c3c9a8558c766febb29db295d2a76bf4c
eduramme <eduramme@gmail.com>
2024-06-12 14:52:32 -0300

    create userdropdown component

--- components/01-atoms/DappHeader.tsx
+++ components/01-atoms/DappHeader.tsx
@@ -5,69 +5,71 @@ import { UserDropdown } from "@/components/02-molecules/UserDropdown";
 
 export const DappHeader = () => {
   return (
-    <div className="w-full h-20 py-5 bg-white px-6 flex justify-between items-center shadow z-50">
-      <Link
-        href="/"
-        className="flex items-center justify-center gap-2.5 shadow-2xl"
-      >
-        <div className="h-6 w-6 bg-gradient-ens rounded-full" />
-        <p className="text-base font-bold text-black">DomainResolver</p>
-      </Link>
-      <div>
-        <ConnectButton.Custom>
-          {({
-            account,
-            chain,
-            openChainModal,
-            openConnectModal,
-            authenticationStatus,
-            mounted,
-          }) => {
-            const ready = mounted && authenticationStatus !== "loading";
-            const connected =
-              ready &&
-              account &&
-              chain &&
-              authenticationStatus === "authenticated";
+    <div className="w-full h-20 py-5 bg-white px-6 flex justify-center items-center shadow z-50">
+      <div className="w-full max-w-[1216px] flex justify-between items-center ">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2.5 shadow-2xl"
+        >
+          <div className="h-6 w-6 bg-gradient-ens rounded-full" />
+          <p className="text-base font-bold text-black">DomainResolver</p>
+        </Link>
+        <div>
+          <ConnectButton.Custom>
+            {({
+              account,
+              chain,
+              openChainModal,
+              openConnectModal,
+              authenticationStatus,
+              mounted,
+            }) => {
+              const ready = mounted && authenticationStatus !== "loading";
+              const connected =
+                ready &&
+                account &&
+                chain &&
+                authenticationStatus === "authenticated";
 
-            if (!connected) {
-              return (
-                <Button
-                  onClick={(e: any) => {
-                    openConnectModal();
-                    e.preventDefault();
-                  }}
-                  size="small"
-                  colorStyle="blueSecondary"
-                  prefix={<WalletSVG />}
-                >
-                  Connect
-                </Button>
-              );
-            }
+              if (!connected) {
+                return (
+                  <Button
+                    onClick={(e: any) => {
+                      openConnectModal();
+                      e.preventDefault();
+                    }}
+                    size="small"
+                    colorStyle="blueSecondary"
+                    prefix={<WalletSVG />}
+                  >
+                    Connect
+                  </Button>
+                );
+              }
 
-            const unsupportedChainClassName = `inline-flex w-auto flex-shrink-0 appearance-none items-center justify-center space-x-2 rounded-md px-5 py-2.5`;
+              const unsupportedChainClassName = `inline-flex w-auto flex-shrink-0 appearance-none items-center justify-center space-x-2 rounded-md px-5 py-2.5`;
 
-            if (chain.unsupported) {
-              return (
-                <button
-                  onClick={(e: any) => {
-                    openChainModal();
-                    e.preventDefault();
-                  }}
-                  type="button"
-                  className={unsupportedChainClassName}
-                >
-                  <span className="flex-shrink-0 text-sm font-medium">
-                    Unsupported network
-                  </span>
-                </button>
-              );
-            }
+              if (chain.unsupported) {
+                return (
+                  <button
+                    onClick={(e: any) => {
+                      openChainModal();
+                      e.preventDefault();
+                    }}
+                    type="button"
+                    className={unsupportedChainClassName}
+                  >
+                    <span className="flex-shrink-0 text-sm font-medium">
+                      Unsupported network
+                    </span>
+                  </button>
+                );
+              }
 
-            return <UserDropdown />;
-          }}
-        </ConnectButton.Custom>
+              return <UserDropdown />;
+            }}
+          </ConnectButton.Custom>
+        </div>
       </div>
     </div>
   );
