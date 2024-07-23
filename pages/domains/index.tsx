04ba87c4a432412af14efb5496a83129af244dd8
eduramme <eduramme@gmail.com>
2024-07-23 19:39:29 -0300

    feat: update MyDomains page overflow handling and background color

--- pages/domains/index.tsx
+++ pages/domains/index.tsx
@@ -32,9 +32,9 @@ export default function RegisterNamePage() {
   }, [address]);
 
   return (
-    <div className="w-full text-black px-5 flex h-full flex-col items-center justify-start bg-white">
+    <div className="w-full text-black px-5 flex h-full flex-col items-center justify-start">
       <div className="w-full flex-col gap-8 py-10 flex justify-start max-w-[1216px]">
-        <Skeleton className="!w-full h-[500px]" loading={isLoading}>
+        <Skeleton className="!w-full min-h-[500px]" loading={isLoading}>
           {!address && !isLoading ? (
             <div className="w-full flex flex-col gap-4 items-center justify-center">
               <Heading level="2" className="text-start text-[26px]">
