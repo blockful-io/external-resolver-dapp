d7199ad5d84c45cc394da15c2b2aa6d160ac14ab
eduramme <eduramme@gmail.com>
2024-07-29 13:04:03 -0300

    feat: create HeaderLink component

--- components/01-atoms/DappHeader.tsx
+++ components/01-atoms/DappHeader.tsx
@@ -5,6 +5,8 @@ import { DEFAULT_CHAIN_ID } from "@/lib/wallet/chains";
 import { UserDropdown } from "@/components/02-molecules";
 import { ConnectMetamask } from "./ConnectMetamask";
 import { PlusCircleIcon } from "./icons/plus-circle-icon";
+import cc from "classcat";
+import { isAction } from "redux";
 
 export const DappHeader = () => {
   const { address, chain } = useAccount();
@@ -22,17 +24,21 @@ export const DappHeader = () => {
   }, []);
 
   return (
-    <div className="w-full h-20 py-5 bg-white px-6 flex justify-between items-center shadow z-50">
-      <div className="w-full max-w-[1216px] flex justify-between items-center mx-auto">
-        <div className="flex items-center">
+    <div className="w-full h-20 bg-white px-6 flex justify-between items-center shadow z-50">
+      <div className="w-full max-w-[1216px] flex justify-between items-center mx-auto h-full">
+        <div className="flex items-center h-full gap-4">
           <Link href="/" className="flex items-center justify-center gap-2.5">
             <p className="text-2xl font-bold text-black">nameful</p>
-            <div></div>
           </Link>
-          <Link className="flex items-center gap-1" href="/domains">
-            <PlusCircleIcon />
-            Manage
-          </Link>
+          <div className="w-1 h-5 grow border-l border-gray-300" />
+          <div className="flex items-center h-full">
+            <HeaderLink
+              label="Register"
+              icon={PlusCircleIcon}
+              isActive={true}
+            />
+            <HeaderLink label="Manage" icon={PlusCircleIcon} isActive={true} />
+          </div>
         </div>
 
         <div>
@@ -40,5 +46,26 @@ export const DappHeader = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+interface HeaderLinkProps {
+  icon: React.ElementType;
+  label: string;
+  isActive: boolean;
+}
+
+const HeaderLink = ({ icon: Icon, label, isActive }: HeaderLinkProps) => {
+  return (
+    <Link
+      className={cc([
+        "flex items-center h-full px-3 gap-2 font-medium hover:bg-gray-50 transition-colors duration-300",
+        { "border-blue-500 border-b-2": isActive },
+      ])}
+      href="/domains"
+    >
+      <Icon />
+      {label}
+    </Link>
   );
 };
