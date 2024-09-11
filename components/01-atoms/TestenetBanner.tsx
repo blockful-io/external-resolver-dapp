316c1a29d9e1acfc5a65743a1b571d83720377b2
eduramme <eduramme@gmail.com>
2024-09-11 14:20:37 -0300

    feat: create testnet banner

---
+++ components/01-atoms/TestenetBanner.tsx
@@ -0,0 +1,7 @@
+export const TestenetBanner = () => {
+  return (
+    <div className="bg-yellow-100 w-full p-2 flex items-center justify-center">
+      You are viewing the Nameful app on Sepolia testnet.
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ import { EnsDomainStatus } from "@/types/ensDomainStatus";
 import { domainWithEth, stringHasMoreThanOneDot } from "@/lib/utils/formats";
 import { DomainStatus } from "@/components/02-molecules";
 import toast from "react-hot-toast";
+import { useAccount } from "wagmi";
+import { sepolia } from "viem/chains";
+import { TestenetBanner } from "@/components/01-atoms/TestenetBanner";
 
 export default function Home() {
   const router = useRouter();
@@ -18,6 +21,12 @@ export default function Home() {
   const [domainStatus, setDomainStatus] = useState<EnsDomainStatus>(
     EnsDomainStatus.Invalid
   );
+
+  const { chain } = useAccount();
+
+  const isConnectedToTestnet = () => {
+    return !!chain && chain.id === sepolia.id;
+  };
 
   const checkDomainAvailability = async (ensName: ENSName) => {
     try {
@@ -84,70 +93,74 @@ export default function Home() {
   };
 
   return (
-    <div className="flex relative h-screen flex-col items-center justify-center bg-white p-4 overflow-clip">
-      <HomepageBg />
+    <>
+      {isConnectedToTestnet() && <TestenetBanner />}
+      <div className="flex relative h-screen flex-col items-center justify-center bg-white p-4 overflow-clip">
+        <HomepageBg />
 
-      <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-y-8 -translate-y-1/3">
-        <div className="flex items-center justify-center gap-2 px-3 py-2 rounded-full border border-gray-200">
-          <p>🔎</p>
-          <p className="text-gray-500 text-xl">Search domains</p>
-        </div>
-        <div className="text-center">
-          <h1 className="font-semibold text-5xl pb-2 text-[#1E2122] mb-4">
-            Simplify your <span className="text-gradient-ens">web3 domain</span>{" "}
-            registration
-          </h1>
-          <p className="text-gray-500 text-xl font-normal">
-            Type in your desired domain and see what&apos;s available.
-          </p>
-        </div>
+        <div className="flex w-full max-w-6xl flex-col items-center justify-center gap-y-8 -translate-y-1/3">
+          <div className="flex items-center justify-center gap-2 px-3 py-2 rounded-full border border-gray-200">
+            <p>🔎</p>
+            <p className="text-gray-500 text-xl">Search domains</p>
+          </div>
+          <div className="text-center">
+            <h1 className="font-semibold text-5xl pb-2 text-[#1E2122] mb-4">
+              Simplify your{" "}
+              <span className="text-gradient-ens">web3 domain</span>{" "}
+              registration
+            </h1>
+            <p className="text-gray-500 text-xl font-normal">
+              Type in your desired domain and see what&apos;s available.
+            </p>
+          </div>
 
-        {/* Input  */}
-        <div className="w-full h-[52px] relative max-w-[470px] ">
-          <div className="absolute top-0 left-0 w-full border border-gray-200 rounded-xl overflow-hidden">
-            <div className="flex w-full justify-center items-center p-2 pl-5">
-              <DebounceInput
-                minLength={3}
-                value={domain}
-                debounceTimeout={300}
-                onChange={handleInputChange}
-                onKeyDown={(e) => goToRegisterPage(e)}
-                className="w-full py-2 text-black text-xl"
-                placeholder="Search for a domain"
-              />
+          {/* Input  */}
+          <div className="w-full h-[52px] relative max-w-[470px] ">
+            <div className="absolute top-0 left-0 w-full border border-gray-200 rounded-xl overflow-hidden">
+              <div className="flex w-full justify-center items-center p-2 pl-5">
+                <DebounceInput
+                  minLength={3}
+                  value={domain}
+                  debounceTimeout={300}
+                  onChange={handleInputChange}
+                  onKeyDown={(e) => goToRegisterPage(e)}
+                  className="w-full py-2 text-black text-xl"
+                  placeholder="Search for a domain"
+                />
 
-              <div className="flex items-center justify-center gap-2">
-                {!!domain && (
-                  <button
-                    className="h-full p-2 hover:-translate-y-[1px] transition-all duration-200"
-                    onClick={clearDomainSearch}
-                  >
-                    <CrossCircleSVG className="text-gray-400 h-4 w-4" />
-                  </button>
-                )}
+                <div className="flex items-center justify-center gap-2">
+                  {!!domain && (
+                    <button
+                      className="h-full p-2 hover:-translate-y-[1px] transition-all duration-200"
+                      onClick={clearDomainSearch}
+                    >
+                      <CrossCircleSVG className="text-gray-400 h-4 w-4" />
+                    </button>
+                  )}
+                </div>
               </div>
-            </div>
-            <div
-              className={`transition-all duration-300 hover:bg-gray-50 overflow-hidden ${
-                domain
-                  ? "opacity-100 visible h-[52px]"
-                  : "opacity-0 invisible h-0"
-              }`}
-            >
-              <Link
-                href={getDomainRedirectionRoute()}
-                className="flex w-full justify-between  bg-transparent items-center border-gray-200 border-t p-4 pl-5"
+              <div
+                className={`transition-all duration-300 hover:bg-gray-50 overflow-hidden ${
+                  domain
+                    ? "opacity-100 visible h-[52px]"
+                    : "opacity-0 invisible h-0"
+                }`}
               >
-                <p className="text-gray-500 min-h-6">
-                  {domain && domainWithEth(domain)}
-                </p>
-                <DomainStatus status={domainStatus} />
-              </Link>
+                <Link
+                  href={getDomainRedirectionRoute()}
+                  className="flex w-full justify-between  bg-transparent items-center border-gray-200 border-t p-4 pl-5"
+                >
+                  <p className="text-gray-500 min-h-6">
+                    {domain && domainWithEth(domain)}
+                  </p>
+                  <DomainStatus status={domainStatus} />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
+        <div className="w-[85%] h-[243px] blur-[125px] bg-gradient-ens absolute bottom-[-200px] rounded-ellipse"></div>
       </div>
-      <div className="w-[85%] h-[243px] blur-[125px] bg-gradient-ens absolute bottom-[-200px] rounded-ellipse"></div>
-    </div>
+    </>
   );
 }
