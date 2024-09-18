4b10b298057d6911fb6856f92962c5bdc2371003
Eduardo <eduramme@gmail.com>
2024-09-18 13:37:57 -0300

    Update components/organisms/ChainChecker.tsx
    Co-authored-by: caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>

--- components/organisms/ChainChecker.tsx
+++ components/organisms/ChainChecker.tsx
@@ -7,7 +7,6 @@ const ChainChecker = () => {
   const router = useRouter();
 
   useEffect(() => {
-    console.log("Hello from ChainChecker", chain);
 
     if (address && !chain?.id) {
       router.push("/not-supported");
