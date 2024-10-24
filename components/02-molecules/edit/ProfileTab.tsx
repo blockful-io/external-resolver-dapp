993ff09b005f2fc2a5957a939c8b617907d1c17a
eduramme <eduramme@gmail.com>
2024-10-24 17:22:34 -0300

    feat: show avatar on user Records modal

--- components/02-molecules/edit/ProfileTab.tsx
+++ components/02-molecules/edit/ProfileTab.tsx
@@ -3,21 +3,39 @@ import { Input, Textarea } from "@ensdomains/thorin";
 import React from "react";
 import { useFields } from "./FieldsContext";
 import Avatar from "boring-avatars";
+import Image from "next/image";
 
 export const ProfileTab = () => {
   const { profileFields, updateField } = useFields();
+  const avatarSrc = profileFields.find(
+    (field) => field.label === "avatar",
+  )?.value;
 
   return (
     <div className="w-full">
       <div className="flex h-[120px] w-full items-end justify-center rounded-xl bg-gradient-ens">
-        <div className="translate-y-6 overflow-hidden rounded-lg border-2 border-white">
-          <Avatar
-            size={80}
-            square
-            name="Margaret Bourke"
-            variant="beam"
-            colors={["#44BCF0", "#7298F8", "#A099FF", "#FFFFFF"]}
-          />
+        <div className="translate-y-6 overflow-hidden rounded-lg">
+          {avatarSrc ? (
+            <Image
+              alt="avatar image"
+              width={100}
+              loading="lazy"
+              height={100}
+              src={avatarSrc}
+              placeholder="empty"
+              className="h-[100px] w-[100px] rounded-[10px] border-4 border-white"
+            />
+          ) : (
+            <div className="h-[100px] w-[100px] overflow-hidden rounded-[10px] border-4 border-white bg-gradient-ens">
+              <Avatar
+                size={100}
+                square
+                name="Margaret Bourke"
+                variant="marble"
+                colors={["#44BCF0", "#7298F8", "#A099FF", "#FFFFFF"]}
+              />
+            </div>
+          )}
         </div>
       </div>
       <div className="flex flex-col gap-4">
