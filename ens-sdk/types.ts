1574567de95474109fe8ca06c0153b5b18e88c86
eduramme <eduramme@gmail.com>
2024-11-28 16:25:49 -0300

    remove unused import

--- ens-sdk/types.ts
+++ ens-sdk/types.ts
@@ -1,5 +1,4 @@
 import { ClientWithEns } from "@ensdomains/ensjs/dist/types/contracts/consts";
-import { ENSName } from "@namehash/ens-utils";
 import { PublicClient } from "viem";
 
 export type EnsPublicClient = PublicClient & ClientWithEns;
